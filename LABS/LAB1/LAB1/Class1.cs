2e03f1f21ccce38d130eb07c1adf979d23341e7c
Bryan Moreno <hbmorenog@gmail.com>
2021-04-25 22:51:02 -0500

    First push

---
+++ LABS/LAB1/LAB1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+using AcAp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace LAB1
+{
+    public class Commands
+    {
+        [CommandMethod("HelloWorld")]
+        public void helloworld()
+        {
+            var doc = AcAp.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var ed = doc.Editor;
+
+            ed.WriteMessage("Hello World");
+        }
+    }
+}
